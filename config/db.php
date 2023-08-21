37e070669176e8adb45a5190a60fa9830f8bcc24
Notko <83171765+Notko@users.noreply.github.com>
2023-08-21 21:39:23 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
-$dbname = "brauemi";
-$username = "brauemi";
-$pass = "brauemi3";
-$dsn = "mysql:host=lab.uzlabina.cz;dbname=$dbname;port=3306";
-//$dsn = "mysql:host=localhost;dbname=$dbname;port=3336";
+$dbname = "dbname";
+$username = "username";
+$pass = "password";
+$host = "localhost";
+$dsn = "mysql:host=$localhost;dbname=$dbname;port=3306";
+
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
